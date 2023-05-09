da76a78f0ea27061fc165236bc47de26851cf752
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-05-09 22:44:21 +0900

    API経由でチャット実行

--- python_starter/main.py
+++ python_starter/main.py
@@ -2,14 +2,23 @@ import application_setting.my_logger as mylogger
 from logging import Logger
 import application_setting.my_credential as mycredential
 from typing import Optional
+import openai
 
 # loggerを取得
 logger: Logger = mylogger.get_logger("main")
 
 
 def main():
-    logger.info("hello, world.")
-    print(mycredential.KEY)
+    openai.api_key = mycredential.OPENAI_API_KEY
+    print(mycredential.OPENAI_API_KEY)
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "あなたは賢いAIです。"},
+            {"role": "user", "content": "1たす1は？"},
+        ],
+    )
+    print(response["choices"][0]["message"]["content"])
 
 
 def add_one(number: int) -> int:
