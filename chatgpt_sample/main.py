6339f2372b89d4755bab91545b6e17b9bc161776
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-05-11 08:44:45 +0900

    langchainからOpenAIのAPIを利用する

--- chatgpt_sample/main.py
+++ chatgpt_sample/main.py
@@ -3,21 +3,22 @@ from logging import Logger
 import application_setting.my_credential as mycredential
 from typing import Optional
 import openai
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import SystemMessage, HumanMessage
 
 # loggerを取得
 logger: Logger = mylogger.get_logger("main")
 
 
 def main():
-    openai.api_key = mycredential.OPENAI_API_KEY
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "あなたは賢いAIです。"},
-            {"role": "user", "content": "1たす1は？"},
-        ],
+    chat = ChatOpenAI(model_name="gpt-3.5-turbo")
+    response = chat(
+        [
+            SystemMessage(content="日本語で回答して。"),
+            HumanMessage(content="ITエンジニアについて30文字で教えて。"),
+        ]
     )
-    logger.info(response["choices"][0]["message"]["content"])
+    logger.info(response)
 
 
 def add_one(number: int) -> int:
