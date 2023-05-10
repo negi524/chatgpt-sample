e003fdbfa6b43414c3cbeabd1f7985c56efd0a66
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-05-11 08:53:05 +0900

    プロンプトのテンプレートを利用する

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,11 @@
-from chatgpt_sample.main import add_one
+from chatgpt_sample.main import create_prompt_template
+from langchain import PromptTemplate
 
 
-def test_add_one():
-    assert add_one(1) == 2
+def test_create_prompt_template():
+    template = """
+    {subject}について30文字で教えて。
+    """
+    assert create_prompt_template() == PromptTemplate(
+        template=template, input_variables=["subject"]
+    )
