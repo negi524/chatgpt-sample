467f24fd341cd5fbb2616ed1f06d1376ea5f4fac
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-05-11 09:05:28 +0900

    LangChainを利用する (#3)
    * poetry add langchain
    
    * langchainからOpenAIのAPIを利用する
    
    * プロンプトのテンプレートを利用する

--- chatgpt_sample/main.py
+++ chatgpt_sample/main.py
@@ -1,27 +1,38 @@
 import application_setting.my_logger as mylogger
 from logging import Logger
 import application_setting.my_credential as mycredential
-from typing import Optional
-import openai
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import SystemMessage, HumanMessage
 
 # loggerを取得
 logger: Logger = mylogger.get_logger("main")
 
 
 def main():
-    openai.api_key = mycredential.OPENAI_API_KEY
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "あなたは賢いAIです。"},
-            {"role": "user", "content": "1たす1は？"},
-        ],
+    prompt = create_prompt_template()
+    prompt_text = prompt.format(subject="ITエンジニア")
+
+    chat = ChatOpenAI(model_name="gpt-3.5-turbo")
+    response = chat(
+        [
+            SystemMessage(content="日本語で回答して。"),
+            HumanMessage(content=prompt_text),
+        ]
     )
-    logger.info(response["choices"][0]["message"]["content"])
+    logger.info(response)
 
 
-def add_one(number: int) -> int:
-    return number + 1
+def create_prompt_template() -> PromptTemplate:
+    """
+    プロンプトのテンプレートを生成する
+    """
+
+    template = """
+    {subject}について30文字で教えて。
+    """
+
+    return PromptTemplate(template=template, input_variables=["subject"])
 
 
 if __name__ == "__main__":
